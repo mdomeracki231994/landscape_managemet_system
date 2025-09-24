d2b85670eab3611ecc1faa9dec679f4607e846f9
Mark Domeracki <mark@johnankerberg.org>
2025-09-24 17:22:53 -0400

    Adding revised files

--- Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47482e9c99c14a56a0530db8166b3d7404884fe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
